51d246d70ed32aa32073621ec58050a8029423dd
Benja Netan <netanyahubenjamin70@gmail.com>
2025-04-17 12:36:02 +0300

    feat: implement basic chat functionality with WebSocket

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,94 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="chat-app">
+    <h1>LAN Chat App</h1>
+    <div :class="['status', { connected }]"></div>
+    <div class="chat-box">
+      <div v-for="(msg, index) in messages" :key="index" class="message">{{ msg }}</div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Type a message..." />
+    <button @click="sendMessage">Send</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const newMessage = ref('');
+const messages = ref([]);
+const connected = ref(false);
+
+let socket = null;
+
+onMounted(() => {
+  socket = new WebSocket('ws://localhost:3000');
+
+  socket.onopen = () => {
+    connected.value = true;
+  };
+
+  socket.onmessage = event => {
+    messages.value.push(event.data);
+  };
+
+  socket.onclose = () => {
+    connected.value = false;
+  };
+});
+
+onBeforeUnmount(() => {
+  socket && socket.close();
+});
+
+function sendMessage() {
+  if (newMessage.value.trim() !== '' && connected.value) {
+    socket.send(newMessage.value);
+    newMessage.value = '';
+  }
+}
+</script>
+
+<style>
+.chat-app {
+  max-width: 400px;
+  margin: 2rem auto;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  font-family: Arial, sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.status {
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  margin-bottom: 10px;
+  background: red;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.status.connected {
+  background: green;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.chat-box {
+  border: 1px solid #eee;
+  height: 200px;
+  overflow-y: auto;
+  margin-bottom: 10px;
+  padding: 5px;
+  background: #f9f9f9;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.message {
+  margin-bottom: 5px;
+}
+
+input {
+  width: calc(100% - 70px);
+  padding: 5px;
+}
+
+button {
+  padding: 5px 10px;
+  margin-left: 5px;
 }
 </style>
